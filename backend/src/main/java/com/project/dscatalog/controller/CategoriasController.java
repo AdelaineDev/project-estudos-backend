175fc5f00a2cd6eb9696eb5f387f68d775729dda
Adelaine Santos <adelainesant18@gmail.com>
2022-09-19 15:19:00 -0300

    Adiciona Endpoint

--- backend/src/main/java/com/project/dscatalog/controller/CategoriasController.java
+++ backend/src/main/java/com/project/dscatalog/controller/CategoriasController.java
@@ -6,6 +6,7 @@ import com.project.dscatalog.service.CategoriasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,12 @@ public class CategoriasController {
 
         return ResponseEntity.ok().body(list);
     }
+    @GetMapping(value = "{id}")
+    public ResponseEntity <CategoriaDTO> findById(@PathVariable Long id) {
+        CategoriaDTO dto = categoriasService.findById(id);
 
+        return ResponseEntity.ok().body(dto);
 
+}
 }
 //ResponseEntity - Objeto que encapsula um metodo http (é um generic)
--- backend/src/main/java/com/project/dscatalog/service/CategoriasService.java
+++ backend/src/main/java/com/project/dscatalog/service/CategoriasService.java
@@ -6,18 +6,30 @@ import com.project.dscatalog.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class CategoriasService {
     @Autowired
     private CategoryRepository categoryRepository;
-
+        @Transactional
     public List<CategoriaDTO> findAll(){
         List<Categorias> list= categoryRepository.findAll();
         return list.stream().map(x -> new  CategoriaDTO(x)).collect(Collectors.toList());
 
 
     }
+    @Transactional
+    public CategoriaDTO findById(Long id){
+        Optional<Categorias> obj = categoryRepository.findById(id);
+        Categorias entidade = obj.get();
+        return new CategoriaDTO(entidade);
+
+
+    }
+
+
 }
